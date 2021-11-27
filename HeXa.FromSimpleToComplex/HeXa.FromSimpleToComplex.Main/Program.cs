95e8bbb61acc4e5b72644ad39c0ec33c7a2bb64e
Vizeo <ebegunov@gmail.com>
2021-11-27 21:39:42 +0200

    Добавьте файлы проекта.

---
+++ HeXa.FromSimpleToComplex/HeXa.FromSimpleToComplex.Main/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HeXa.FromSimpleToComplex.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        
+        
+        
+        }
+    }
+}
